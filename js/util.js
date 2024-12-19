2dac44814c457b5a541ba2143aff686dcb5e1a50
Olga Gulyakevich <olga.gulyakevi4@gmail.com>
2024-12-19 22:01:23 +0100

    feat: добавлена функциональность кнопки «Загрузить ещё» для комментариев

--- js/util.js
+++ js/util.js
@@ -30,11 +30,37 @@ const clearElement = (element) => {
   element.innerHTML = '';
 };
 
+/**
+ * Функция склонения слов в зависимости от числительного
+ * @param {number} count - Число, для которого нужно склонить слово
+ * @param {Array} forms - Массив форм слова [1, 2-4, 5-0]
+ * @returns {string}  - Правильная форма слова
+ */
+const getDeclineForm = (count, forms) => {
+  const mod10 = count % 10;
+  const mod100 = count % 100;
+
+  if (mod100 >= 11 && mod100 <= 19) {
+    return forms[2];
+  }
+
+  if (mod10 === 1) {
+    return forms[0];
+  }
+
+  if (mod10 >= 2 && mod10 <= 4) {
+    return forms[1];
+  }
+
+  return forms[2];
+};
+
 export {
   getIdGenerator,
   getRandomInteger,
   getRandomArrayElement,
   isEscapeKey,
   toggleClass,
-  clearElement
+  clearElement,
+  getDeclineForm
 };
